3f29c633bcf0def5b80dd7c1dd971ab73a83806f
Yunfei Ouyang <614527349oyyf@gmail.com>
2020-12-09 18:21:36 -0500

    Added options for 3 labels KNN

--- KNN.py
+++ KNN.py
@@ -29,7 +29,7 @@ def hog(image_face):
     numBins = 9
     hog_descriptor = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, numBins)
     h = hog_descriptor.compute(image_face)
-    return sum(h)
+    return h
 
 
 def face_detect(image_face):
@@ -46,33 +46,40 @@ def face_detect(image_face):
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.2, 3)
         hog_eye = 0
         for (ex, ey, ew, eh) in eyes:
-            hog_eye += hog(img[ex: ey, ex + ew: ey + eh])
+            hog_eye += hog(img[ex: ex + ew, ey: ey + eh])
             cv2.rectangle(roi_gray, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
         # mouth
         mouth = mouth_cascade.detectMultiScale(roi_gray, 1.5, 5)
         hog_mouse = 0
         for (mx, my, mw, mh) in mouth:
-            hog_mouse += hog(img[mx: my, mx + mw: my + mh])
+            hog_mouse += hog(img[mx: mx + mw, my: my + mh])
             cv2.rectangle(roi_gray, (mx, my), (mx + mw, my + mh), (0, 0, 255), 2)
 
         # nose
         nose = nose_cascade.detectMultiScale(roi_gray, 1.2, 5)
         hog_nose = 0
         for (nx, ny, nw, nh) in nose:
-            hog_nose += hog(img[nx: ny, nx + nw: ny + nh])
+            hog_nose += hog(img[nx: nx + nw, ny: ny + nh])
             cv2.rectangle(roi_gray, (nx, ny), (nx + nw, ny + nh), (255, 0, 255), 2)
 
         total_hog += hog_eye + hog_mouse + hog_nose
 
     # plt.imshow(img)
     # plt.show()
-    print(float(total_hog))
+
     return float(total_hog)
 
 
-def load_data_name_label():
-    """Loads the name and the corresponding label from the csv file."""
+def hog_face_diff(image_face, default_hog):
+    img_face = cv2.imread(image_face)
+    image_face_hog = hog(img_face[50:300, 50:300])
+    hog_diff_euclidean_norm = np.linalg.norm(default_hog - image_face_hog)
+    return hog_diff_euclidean_norm
+
+
+def load_data_label():
+    """Loads the data and the corresponding label from the csv file."""
     with open("500_picts_satz.csv") as f:
         lines = [line.split() for line in f]  # create a list of lists
 
@@ -81,15 +88,36 @@ def load_data_name_label():
     return data_arr
 
 
-def load_data():
+def load_data_label_3_label():
+    """Loads the data and the corresponding label from the csv file. Only keep label happiness, neutral, and anger"""
+    with open("500_picts_satz.csv") as f:
+        lines = [line.split() for line in f]  # create a list of lists
+
+    lines.pop(0)
+    data_arr = np.char.split(lines, sep=',').flatten()
+    filtered_arr = []
+
+    for i in data_arr:
+        if i[2] == "happiness" or i[2] == "neutral" or i[2] == "anger":
+            filtered_arr.append(i)
+
+    filtered_arr = np.array(filtered_arr)
+    return filtered_arr
+
+
+def load_data(choice):
     """Loads the gray scale image data and then split the entire database randomly
     into 75% train, 15% validation, 15% test"""
-    data_label = load_data_name_label()
+    Default_Face = cv2.imread('og_500/Spencer_Abraham_0003.jpg')
+    Default_Face_Hog = hog(Default_Face[50:300, 50:300])
+    if choice == 0:
+        data_label = load_data_label()
+    else:  # filtered data
+        data_label = load_data_label_3_label()
     data = []
     labels = []
     for i in data_label:
-        img = cv2.imread("og_500/"+str(i[1]))
-        data.append(hog(img))
+        data.append([hog_face_diff("og_500/" + str(i[1]), Default_Face_Hog)])
         if i[2] == 'fear':
             labels.append(0)
         if i[2] == 'happiness':
@@ -112,8 +140,8 @@ def load_data():
     return X_train, Y_train, X_validate, Y_validate, X_test, Y_test
 
 
-def select_knn_model():
-    X_train, Y_train, X_validate, Y_validate, X_test, Y_test = load_data()
+def select_knn_model(choice):
+    X_train, Y_train, X_validate, Y_validate, X_test, Y_test = load_data(choice)
 
     best_k = -1
     best_score = 0.0
@@ -145,8 +173,27 @@ def select_knn_model():
     plt.xlabel("K number of nearest neighbours")
     plt.ylabel("training/validation accuracy")
     plt.legend(["training accuracy", "validation accuracy"])
+    if choice == 0:
+        plt.title("KNN accuracy with 5 labels")
+    else:   # choice == 1
+        plt.title("KNN accuracy with 3 labels: happiness, neutral, anger.")
     plt.show()
 
 
 if __name__ == "__main__":
-    select_knn_model()
+    select_knn_model(0)
+    select_knn_model(1)
+    # img = cv2.imread("og_500/Sourav_Ganguly_0003.jpg")
+    # #face
+    # cv2.rectangle(img, (50, 50), (300, 300), (255, 0, 255), 2)
+    # #eye
+    # cv2.rectangle(img, (75, 125), (150, 170), (255, 0, 255), 2)
+    # cv2.rectangle(img, (200, 125), (275, 170), (255, 0, 255), 2)
+    # #nose
+    # cv2.rectangle(img, (200, 125), (150, 225), (255, 0, 255), 2)
+    # #mouth
+    # cv2.rectangle(img, (125, 250), (225, 280), (255, 0, 255), 2)
+    # plt.imshow(img)
+    # plt.show()
+    # plt.imshow(img[50:300, 50:300])
+    # plt.show()
