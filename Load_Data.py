da73688f1bee7d9ac6acff0f199e571412d1ff8c
Yunfei Ouyang <614527349oyyf@gmail.com>
2020-12-18 22:16:11 -0500

    removed some junk

--- Load_Data.py
+++ Load_Data.py
@@ -5,70 +5,6 @@ import numpy as np
 import cv2
 import dlib
 
-# face
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-face_cascade.load('haarcascades/haarcascade_frontalface_default.xml')
-# eye
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-eye_cascade.load('haarcascades/haarcascade_eye.xml')
-# mouth
-mouth_cascade = cv2.CascadeClassifier('haarcascade_mcs_mouth.xml')
-mouth_cascade.load('haarcascades/haarcascade_mcs_mouth.xml')
-# nose
-nose_cascade = cv2.CascadeClassifier('haarcascade_mcs_nose.xml')
-nose_cascade.load('haarcascades/haarcascade_mcs_nose.xml')
-
-
-def face_detect(img):
-    faces = face_cascade.detectMultiScale(img, 1.2, 3)
-    facial_landmark = np.zeros((5,))
-    if len(faces) >= 1:
-        facial_landmark[0] = faces[0][3]
-
-    for (x, y, w, h) in faces:
-        img_face = img[y:y + h, x:x + w]
-        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        plt.imshow(img_face)
-        plt.show()
-
-        # eye
-        img_eye = img_face[y: y + h // 2, x: x + w]
-        plt.imshow(img_eye)
-        plt.show()
-        eyes = eye_cascade.detectMultiScale(img_eye, 1.2, 3)
-        if len(eyes) >= 2:
-            eyes = eyes[:2]
-            facial_landmark[1], facial_landmark[2] = eyes[0][3], eyes[1][3]
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(img_face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-
-        # mouth
-        img_mouth = img_face[y + h // 2: y + h, x: x + w]
-        plt.imshow(img_mouth)
-        plt.show()
-        mouth = mouth_cascade.detectMultiScale(img_mouth, 1.5, 5)
-        if len(mouth) >= 1:
-            mouth = mouth[:1]
-            facial_landmark[3] = mouth[0][3]
-        for (mx, my, mw, mh) in mouth:
-            cv2.rectangle(img_face, (mx, my + h // 2), (mx + mw, my + mh + h // 2), (0, 0, 255), 2)
-
-        # nose
-        img_nose = img_face[:, x + w // 5: x + 4 * w // 5]
-        plt.imshow(img_nose)
-        plt.show()
-        nose = nose_cascade.detectMultiScale(img_nose, 1.2, 5)
-        if len(nose) >= 1:
-            nose = nose[:1]
-            facial_landmark[4] = nose[0][3]
-        for (nx, ny, nw, nh) in nose:
-            cv2.rectangle(img_face, (nx + w // 5, ny), (nx + nw + 4 * w // 5, ny + nh), (255, 0, 255), 2)
-
-    plt.imshow(img)
-    plt.show()
-
-    return facial_landmark
-
 
 def facial_landmark(image):
     detector = dlib.get_frontal_face_detector()
@@ -153,19 +89,24 @@ def load_data(choice_input_method):
 
         img_face = cv2.imread("og/" + str(i[1]))
 
-        if choice_input_method == 0:  # Gradient
+        if choice_input_method == 0:  # GrayScale
+            data.append([diff(img_face[50:300, 50:300], Default_Face)])
+            if i[2] == 'sadness':
+                img_face_g = gradientMagnitude(cv2.flip(img_face, 1)[50:300, 50:300])
+                data.append([diff(img_face_g, Default_Face_g)])
+        if choice_input_method == 1:  # Gradient
             img_face_g = gradientMagnitude(img_face[50:300, 50:300])
             data.append([diff(img_face_g, Default_Face_g)])
             if i[2] == 'sadness':
                 img_face_g = gradientMagnitude(cv2.flip(img_face, 1)[50:300, 50:300])
                 data.append([diff(img_face_g, Default_Face_g)])
-        elif choice_input_method == 1:  # Edge
+        elif choice_input_method == 2:  # Edge
             image_edge = edge_detect(img_face[50:300, 50:300])
             data.append([diff(image_edge, Default_Face_edge)])
             if i[2] == 'sadness':
                 image_edge = edge_detect(cv2.flip(img_face, 1)[50:300, 50:300])
                 data.append([diff(image_edge, Default_Face_edge)])
-        elif choice_input_method == 2:  # HoG
+        elif choice_input_method == 3:  # HoG
             image_HoG = hog(img_face[50:300, 50:300])
             data.append([diff(image_HoG, Default_Face_Hog)])
             if i[2] == 'sadness':
--- README.md
+++ README.md
@@ -8,10 +8,6 @@ Data is from Kaggle:
 ```
 https://www.kaggle.com/c/emotion-detection-from-facial-expressions/data
 ```
-Haarcascades Model is from OpenCV:  # Note that it is not used in the project. We used dlib instead.
-```
-https://github.com/AlexeyAB/OpenCV-detection-models
-```
 Dlib pretrained model:
 ```
 http://dlib.net/face_landmark_detection.py.html
